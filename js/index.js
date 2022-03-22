dfde3c1d961dcf02144d8f901fcff3a97ba28c25
Jessica Cintra <jessica.cintra0712@gmail.com>
2022-03-21 23:54:01 -0300

    pag de ganhou e perdeu, verificação de idade

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+overAge = function () {
+    $('#age-verify').addClass('hidden');
+  }
+  
+  underAge = function () {
+    $('#age-verify').addClass('under');
+  }
+  
+  goBack = function () {
+      window.history.back();
+  }
